c3dc65217afc112193f71af7389f186cde0a10cd
Abhishek <abhishekc838@gmail.com>
2022-05-11 22:05:54 +0530

    about section doen

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -12,7 +12,7 @@ const AboutUs = () => (
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique ipsum fugit debitis ab. Dolores eaque repellendus quas facere sed nisi.</p>
+        <p className="p__opensans">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique ipsum fugit debitis ab. Dolores eaque repellendus quas facere ipsum sed nisi.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
       <div className="app__aboutus-content_knife flex__center">
@@ -22,7 +22,7 @@ const AboutUs = () => (
         <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique ipsum fugit debitis ab. Dolores eaque repellendus quas facere sed nisi.</p>
+        <p className="p__opensans">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique ipsum fugit debitis ab. Dolores eaque repellendus quas facere ipsum sed nisi.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
     </div>
