73609a29e87b68a6ac13183af9d92f479abd575e
Abhishek <abhishekc838@gmail.com>
2022-05-11 21:14:11 +0530

    header done

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,10 +6,14 @@ import { SubHeading } from '../../components';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading/>
+      <SubHeading title="Chase the new flavour"/>
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{margin: '2rem 0'}}>Sit Tellus Lobortis sed Senectus Vivamus Molestie. Condimentum Volutput Morbi Facilisis Quam Scelerisque Sapien Et. Prenatibus Aliquam Amet Tellus</p>
+      <button type="button" className="custom__buttom">Explore Menu</button>
     </div>
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="img" />
+    </div>
   </div>
 );
 
