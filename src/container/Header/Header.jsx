43c0c861f07dfebe5d7fc5b6d5a01e0d77433c58
Abhishek <abhishekc838@gmail.com>
2022-05-15 21:51:50 +0530

    menu done

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => (
       <SubHeading title="Chase the new flavour"/>
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
       <p className="p__opensans" style={{margin: '2rem 0'}}>Sit Tellus Lobortis sed Senectus Vivamus Molestie. Condimentum Volutput Morbi Facilisis Quam Scelerisque Sapien Et. Prenatibus Aliquam Amet Tellus</p>
-      <button type="button" className="custom__buttom">Explore Menu</button>
+      <button type="button" className="custom__button">Explore Menu</button>
     </div>
     <div className="app__wrapper_img">
       <img src={images.welcome} alt="img" />
